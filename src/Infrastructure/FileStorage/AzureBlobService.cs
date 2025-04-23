3f422a9311499481519cc6d9c3026dcd19350bbe
Kyriaki Darivaki <t8210030@aueb.gr>
2025-04-23 18:01:44 +0300

    connect to azure blob storage

---
+++ src/Infrastructure/FileStorage/AzureBlobService.cs
@@ -0,0 +1,18 @@
+﻿using Azure.Storage;
+using Azure.Storage.Blobs;
+
+namespace Infrastructure.FileStorage
+{
+    public class AzureBlobService
+    {
+        private readonly string _storageAccount = "hirehiveblob";
+        private readonly string _accessKey = "eI42LydCJsBdaT8XtpjfbPBKmz98Gx25yxGVCtbcJPbpGkNbfoNCR/22E5aERjwpfJA+1ToRG2m0+ASt5WiITA==";
+        private readonly BlobServiceClient _blobServiceClient;
+        public AzureBlobService()
+        {
+            var credential = new StorageSharedKeyCredential(_storageAccount, _accessKey);
+            var blobUri = $"https://{_storageAccount}.blob.core.windows.net";
+            _blobServiceClient = new BlobServiceClient(new Uri(blobUri), credential);
+        }
+    }
+}
