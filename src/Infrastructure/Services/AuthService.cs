7d545e50eed75176eba5b3650cf94296e1eb05bf
Kyriaki Darivaki <t8210030@aueb.gr>
2025-04-24 17:10:56 +0300

    move service implementation to infrastructure

---
+++ src/Infrastructure/Services/AuthService.cs
@@ -0,0 +1,71 @@
+﻿using HireHive.Application.DTOs.Account;
+using HireHive.Application.Interfaces;
+using HireHive.Domain.Entities;
+using HireHive.Domain.Interfaces;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+
+namespace HireHive.Infrastructure.Services
+{
+    public class AuthService : IAuthService
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+        private readonly IMapper _mapper;
+        private readonly ILogger<AuthService> _logger;
+
+        public AuthService(
+            IUserRepository userRepository,
+            UserManager<User> userManager,
+            SignInManager<User> signInManager,
+            IMapper mapper,
+            ILogger<AuthService> logger)
+        {
+            _userRepository = userRepository;
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _mapper = mapper;
+            _logger = logger;
+        }
+        public async Task Register(RegisterDto registerDto)
+        {
+            var user = await _userManager.FindByEmailAsync(registerDto.Email);
+            if (user != null)
+            {
+                throw new ArgumentException("A user with email {Email} already exists.", registerDto.Email);
+            }
+
+            //todo use ctor of user instead
+            var newUser = _mapper.Map<User>(registerDto);
+            newUser.UserName = registerDto.Email;
+
+            var id = await _userRepository.AddUserAsync(newUser, registerDto.Password);
+        }
+
+        public async Task<bool> ValidateUserCredentialsAsync(string email, string password)
+        {
+            var user = await _userManager.FindByEmailAsync(email);
+            if (user == null)
+            {
+                return false;
+            }
+
+            return await _userManager.CheckPasswordAsync(user, password);
+        }
+
+        public async Task Login(LoginDto userDto)
+        {
+            bool isValid = await ValidateUserCredentialsAsync(userDto.Email, userDto.Password);
+            if (!isValid)
+            {
+                _logger.LogWarning("Login failed for user {Email}.", userDto.Email);
+                throw new UnauthorizedAccessException("Invalid credentials.");
+            }
+
+            //AppUser appUser = _mapper.Map<AppUser>(userDto);
+            //await _signInManager.SignInAsync(user, isPersistent: false);
+            // todo: add jwt tokens 
+        }
+    }
+}
---
+++ src/Infrastructure/Services/UserService.cs
@@ -0,0 +1,72 @@
+﻿using Ardalis.GuardClauses;
+using HireHive.Application.DTOs.User;
+using HireHive.Application.Interfaces;
+using HireHive.Domain.Exceptions;
+using HireHive.Domain.Exceptions.User;
+using HireHive.Domain.Interfaces;
+using Microsoft.Extensions.Logging;
+
+namespace HireHive.Infrastructure.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly ILogger<UserService> _logger;
+        private readonly IMapper _mapper;
+
+        public UserService(
+            IUserRepository userRepository,
+            ILogger<UserService> logger,
+            IMapper mapper)
+        {
+            _userRepository = userRepository;
+            _logger = logger;
+            _mapper = mapper;
+        }
+
+        public async Task Delete(Guid id)
+        {
+            try
+            {
+                var user = await _userRepository.GetByIdAsync(id)
+                                ?? throw new UserNotFoundException();
+
+                _logger.LogInformation("User with id {id} deleted.", id);
+
+                await _userRepository.DeleteUserAsync(id);
+            }
+            catch (BaseException ex)
+            {
+                //todo log
+                throw;
+            }
+
+        }
+
+        public async Task<List<UserDto>> GetAll()
+        {
+            var users = await _userRepository.GetAllUsersAsync();
+
+            return _mapper.Map<List<UserDto>>(users);
+        }
+
+        public async Task<UserDto> GetById(Guid id)
+        {
+            var user = await _userRepository.GetByIdAsync(id)
+                ?? throw new NotFoundException("", $"User with ID {id} was not found.");
+
+            return _mapper.Map<UserDto>(user);
+        }
+
+        public async Task Update(Guid id, UpdateDto userModel)
+        {
+            var userToUpdate = await _userRepository.GetByIdAsync(id)
+                ?? throw new NotFoundException("", $"User with ID {id} was not found.");
+
+            userToUpdate.UpdateUser(userModel.FirstName, userModel.LastName);
+            await _userRepository.UpdateUserAsync(userToUpdate);
+
+            _logger.LogInformation("User with id {id} updated.", id);
+        }
+    }
+}
