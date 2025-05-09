e4b4583bc336a004a6259c5d210febe7cb573b43
Kyriaki Darivaki <t8210030@aueb.gr>
2025-05-09 09:31:04 +0300

    fixes to login + register page

--- front/hirehive/src/features/user/user-routing.module.ts
+++ front/hirehive/src/features/user/user-routing.module.ts
@@ -6,6 +6,7 @@ import { ProfilePageComponent } from './pages/profile/profile-page.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginPageComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'register', component: RegisterPageComponent },
   { path: 'profile', component: ProfilePageComponent }
 ];
