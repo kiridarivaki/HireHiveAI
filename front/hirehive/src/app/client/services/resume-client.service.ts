6392e49ba487d9b6d3ef267cd41d32fe10f82fe5
Kyriaki Darivaki <t8210030@aueb.gr>
2025-05-12 21:46:53 +0300

    add client + shared entity services

--- front/hirehive/src/app/client/services/resume-client.service.ts
+++ front/hirehive/src/app/client/services/resume-client.service.ts
@@ -3,31 +3,39 @@ import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { Resume } from "../models/resume.model";
 import { environment } from "src/environments/environment";
+import { UrlService } from "../helpers/url-service.service";
 
 @Injectable({
     providedIn : 'root'
 })
-export class ResumeClientService{
-    
-    private url = `${environment.apiBaseUrl}/user`;
-
+export class ResumeClientService{   
     constructor (
-        private http : HttpClient
+        private http : HttpClient,
+        private urlService : UrlService
     ){}
 
     getAll() : Observable<Array<Resume>>{
-        return this.http.get<Array<Resume>>(this.url);
+        const getAllUrl = this.urlService.urlFor('Resume', undefined, undefined)
+        return this.http.get<Array<Resume>>(getAllUrl);
     }
 
-    get() : Observable<Resume>{
-        return this.http.get<Resume>(this.url);
+    getById(resumeId: string) : Observable<Resume>{
+        const getResumeUrl = this.urlService.urlFor('resume', undefined, { id: resumeId });
+        return this.http.get<Resume>(getResumeUrl);
     }
 
-    upload(resume: Resume) : void{
-        this.http.post<Resume>(this.url, resume);
+    upload(userId: string, uploadData: FormData) : void{
+        const uploadResumeUrl = this.urlService.urlFor('resume', 'upload/{id}', { id: userId });
+        this.http.patch<Resume>(uploadResumeUrl, uploadData);
     }
 
-    delete(resume: Resume) : void{
-        this.http.post<Resume>(this.url, resume);
+    update(userId: string, updateData: FormData) : void{
+        const updateResumeUrl = this.urlService.urlFor('resume', 'update/{id}', { id: userId });
+        this.http.patch<Resume>(updateResumeUrl, updateData);
+    }
+
+    delete(resumeId: string) : void{
+        const deleteResumeUrl = this.urlService.urlFor('resume', 'delete/{id}', { id: resumeId });
+        this.http.request(deleteResumeUrl, resumeId);
     }
 }
--- front/hirehive/src/app/client/services/user-client.service.ts
+++ front/hirehive/src/app/client/services/user-client.service.ts
@@ -3,31 +3,34 @@ import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { User } from "../models/user.model";
 import { environment } from "src/environments/environment";
+import { UrlService } from "../helpers/url-service.service";
 
 @Injectable({
     providedIn : 'root'
 })
-export class UserClientService{
-    
-    private url = `${environment.apiBaseUrl}/User`;
-
+export class UserClientService{   
     constructor (
-        private http : HttpClient
+        private http : HttpClient,
+        private urlService : UrlService
     ){}
 
     getAll() : Observable<Array<User>>{
-        return this.http.get<Array<User>>(this.url);
+        const getAllUrl = this.urlService.urlFor('user', undefined, undefined)
+        return this.http.get<Array<User>>(getAllUrl);
     }
 
-    get() : Observable<User>{
-        return this.http.get<User>(this.url);
+    getById(userId: string) : Observable<User>{
+        const getUserUrl = this.urlService.urlFor('user', undefined, { id: userId });
+        return this.http.get<User>(getUserUrl);
     }
 
-    upload(User: User) : void{
-        this.http.post<User>(this.url, User);
+    update(userId: string, updateData: FormData) : void{
+        const updateUserUrl = this.urlService.urlFor('user', 'update/{id}', { id: userId });
+        this.http.patch<User>(updateUserUrl, User);
     }
 
-    delete(User: User) : void{
-        this.http.post<User>(this.url, User);
+    delete(userId: string) : void{
+        const deleteUserUrl = this.urlService.urlFor('user', 'delete/{id}', { id: userId });
+        this.http.request(deleteUserUrl, userId);
     }
 }
