136b08abfe206a95437f8c428a7b490f17429a64
Kyriaki Darivaki <t8210030@aueb.gr>
2025-05-19 00:45:04 +0300

    add custom injector to dialog component

--- front/hirehive/src/app/shared/components/dialog/dialog.component.ts
+++ front/hirehive/src/app/shared/components/dialog/dialog.component.ts
@@ -1,18 +1,27 @@
-import { Component, Inject } from "@angular/core";
+import { Component, Inject, Injector } from "@angular/core";
 import { DialogData } from "@shared/models/dialog.model";
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { MatDialogModule } from '@angular/material/dialog';
 import { CommonModule } from "@angular/common";
+import { AppButtonComponent } from "../button/button.component";
 
 @Component({
   selector: 'app-dialog',
-  imports: [MatDialogModule, CommonModule],
+  imports: [MatDialogModule, CommonModule, AppButtonComponent],
   templateUrl: './dialog.component.html',
+  styleUrl: './dialog.component.css'
 })
 export class AppDialogComponent {
+  customInjector: Injector;
+
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: DialogData,
-    public dialogRef: MatDialogRef<AppDialogComponent>
-  ) {}
-  
+    public dialogRef: MatDialogRef<AppDialogComponent>,
+    private injector: Injector
+  ) {
+    this.customInjector = Injector.create({
+      providers: [{ provide: MAT_DIALOG_DATA, useValue: data.data }],
+      parent: injector
+    });
+  }
 }
