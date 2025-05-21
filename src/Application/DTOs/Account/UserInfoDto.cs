4e1c326e6dd1e19f2a2fbb63dcac825ed61b12ec
Kyriaki Darivaki <t8210030@aueb.gr>
2025-05-21 14:01:37 +0300

    add email confirmed to user info

--- src/Application/DTOs/Account/UserInfoDto.cs
+++ src/Application/DTOs/Account/UserInfoDto.cs
@@ -6,6 +6,7 @@ namespace HireHive.Application.DTOs.Account
     {
         public List<string> Roles { get; set; } = null!;
         public string Email { get; set; } = null!;
+        public bool EmailConfirmed { get; set; }
         public string FirstName { get; set; } = null!;
         public string LastName { get; set; } = null!;
         public EmploymentStatus EmploymentStatus { get; set; }
