321fdb1e6dec8e812178b4af60ea1f1275bc4127
Kyriaki Darivaki <t8210030@aueb.gr>
2025-06-02 12:03:43 +0300

    add /api to base url

--- front/hirehive/src/environments/environment.prod.ts
+++ front/hirehive/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiBaseUrl: "https://hirehiveai-dmbuhvemgtf8cjg3.northeurope-01.azurewebsites.net"
+    apiBaseUrl: "https://hirehiveai-dmbuhvemgtf8cjg3.northeurope-01.azurewebsites.net/api"
 };
